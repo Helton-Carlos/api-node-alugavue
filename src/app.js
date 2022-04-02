de2319b8a2e58adbce9bbb822598f6f52c561998
Helton <heltoncarlospa@hotmail.com>
2022-04-02 16:48:09 -0300

    ajuste headers

--- src/app.js
+++ src/app.js
@@ -11,10 +11,10 @@ class App {
     this.app.use(express.json());
 
     this.app.use((req, res, next) => {
-      res.header("Access-Controll-Allow-Origin", "*");
-      res.header("Access-Controll-Allow-Methods", "Get, POST, PUT, DELETE");
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Methods", "Get, POST, PUT, DELETE");
       res.header(
-        "Access-Controll-Allow-Headers",
+        "Access-Control-Allow-Headers",
         "Access, Content-type, Authorization, Acept, Origin, X-Requested-With"
       );
 
