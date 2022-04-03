346a2b8d460d92a96cef528538eafc1c03c629c3
Helton <heltoncarlospa@hotmail.com>
2022-04-03 14:43:45 -0300

    ajuste bairros json

--- src/app/controllers.js
+++ src/app/controllers.js
@@ -1,9 +1,14 @@
 const novidades = require("./json/novidades.json");
-
+const bairros = require("./json/bairros.json");
 class controllers{
     novidades(req,res){
         return res.status(200).json({
          novidades
+        })
+    };
+    bairros(req,res){
+        return res.status(200).json({
+            bairros
         })
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -2,4 +2,5 @@ const {Router} = require('express')
 const routes =new Router()
 const controller = require('../src/app/controllers')
 routes.get("/novidades",controller.novidades)
+routes.get("/bairros",controller.bairros)
 module.exports=routes
