52e8721089e94dbf385d0be6295f0aa24c203669
Helton <heltoncarlospa@hotmail.com>
2022-04-04 22:41:01 -0300

    imoveis

--- src/app/controllers.js
+++ src/app/controllers.js
@@ -1,5 +1,6 @@
 const novidades = require("./json/novidades.json");
 const bairros = require("./json/bairros.json");
+const imoveis = require("./json/imoveis.json");
 class controllers{
     novidades(req,res){
         return res.status(200).json({
@@ -10,7 +11,12 @@ class controllers{
         return res.status(200).json({
             bairros
         })
-    }
+    };
+    imoveis(req,res){
+        return res.status(200).json({
+            imoveis
+        })
+    };
 }
 
 module.exports=new controllers()
--- src/routes.js
+++ src/routes.js
@@ -3,4 +3,5 @@ const routes =new Router()
 const controller = require('../src/app/controllers')
 routes.get("/novidades",controller.novidades)
 routes.get("/bairros",controller.bairros)
+routes.get("/imoveis",controller.imoveis)
 module.exports=routes
